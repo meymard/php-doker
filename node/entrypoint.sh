2aa42df5787c784416824cd7f6915ae50655097f
meymard <contact@marc-eymard.fr>
2017-11-25 10:47:16 +0100

    update react ennv

--- node/entrypoint.sh
+++ node/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 bash
 yarn global add create-react-app
+yarn global add @api-platform/client-generator
 create-react-app .
 chown node:node -R .
+yarn add redux react-redux redux-thunk redux-form react-router-dom react-router-redux prop-types
 yarn install
 yarn start
